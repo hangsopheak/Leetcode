15c6a5b55dca1e2a7298c1afb51622fd8649d186
sow7 <wansongsong.jack@gmail.com>
2015-07-23 21:35:29 -0400

    20

--- src/leetcode/ValidParentheses.java
+++ src/leetcode/ValidParentheses.java
@@ -15,8 +15,8 @@ public class ValidParentheses {
 
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char tmp = s.charAt(i);
+        for (int i=0;i<s.length();i++) {
+            char tmp=s.charAt(i);
             if (tmp == '(') {
                 stack.push(tmp);
             }
