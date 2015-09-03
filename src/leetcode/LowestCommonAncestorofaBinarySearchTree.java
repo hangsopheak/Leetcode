b7d05e21e374689fc9972537335fd626f423c5e1
sow7 <wansongsong.jack@gmail.com>
2015-09-03 15:15:03 -0400

    235

--- src/leetcode/LowestCommonAncestorofaBinarySearchTree.java
+++ src/leetcode/LowestCommonAncestorofaBinarySearchTree.java
@@ -12,20 +12,13 @@ package leetcode;
 public class LowestCommonAncestorofaBinarySearchTree {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
             return root;
         }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) {
-            return root;
-        }
-        if (left != null) {
-            return left;
-        }
-        if (right != null) {
-            return right;
-        }
-        return null;
     }
+
 }
