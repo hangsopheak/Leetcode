8c7e3046b3b7d3ec2dd56e86fc6eb91b32a068e6
sow7 <wansongsong.jack@gmail.com>
2015-09-03 16:05:52 -0400

    dsda

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SONGSONG
+ */
+public class Test {
+
+public static int solution(int[] nums){
+   int tmp=0;
+   for(int i:nums){
+      tmp^=i;
+   }
+   return tmp;
+}
+
+    public static void main(String[] args) {
+        int[] a={677,  677};
+        System.out.print(solution(a));
+    }
+}
