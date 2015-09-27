3444e593b5ac56deb0455d968a39fbf717dbd51b
sow7 <wansongsong.jack@gmail.com>
2015-09-27 01:48:44 -0400

    165

--- src/Test.java
+++ src/Test.java
@@ -21,6 +21,7 @@ public static int solution(int[] nums){
 
     public static void main(String[] args) {
         String a="adasa";
-        System.out.print(a.substring(1, 3));
+        
+        System.out.print(Integer.parseInt("00000"));
     }
 }
