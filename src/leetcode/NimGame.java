803f66a571103a3ae77ab410fd668507aaf0b38d
sow7 <wansongsong.jack@gmail.com>
2015-10-14 20:02:39 -0400

    234

---
+++ src/leetcode/NimGame.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+/**
+ *
+ * @author SONGSONG
+ */
+public class NimGame {
+    public boolean canWinNim(int n) {
+        return n % 4 == 0 ? false : true;
+    }
+}
