6dada71db6fe41fa6a04f700eda217df3700c91d
sow7 <wansongsong.jack@gmail.com>
2015-10-30 17:49:29 -0400

    1

--- src/Test.java
+++ src/Test.java
@@ -13,33 +13,83 @@ import java.util.List;
  *
  * @author SONGSONG
  */
-class ReverseComparator implements Comparator<Integer> {
-
-    public int compare(Integer a, Integer b) {
-        return b - a;
-    }
-}
-
 public class Test {
 
     /**
+     * Return the encode string for input
      *
-     * @param A
+     * @param input
      * @return
      */
-    public static int singleNumber(int[] A) {
-        int ones = 0, twos = 0;
-        for (int i = 0; i < A.length; i++) {
-            ones = (ones ^ A[i]) & ~twos;
-            twos = (twos ^ A[i]) & ~ones;
-            System.out.println(ones + " " + twos);
+    static String Encode(String input) {
+        StringBuilder sb = new StringBuilder();
+        int length = input.length();
+        for (int i = 0; i < length; i++) {
+            char current = input.charAt(i);
+            int current_count = 1;
+            while (i + 1 < length && current == input.charAt(i + 1)) {
+                current_count++;
+                i++;
+            }
+            if (Character.isDigit(current) || current_count > 3) {
+                sb.append(current_count);
+                sb.append('*');
+                sb.append(current);
+            } else {
+                for (int j = 0; j < current_count; j++) {
+                    sb.append(current);
+                }
+            }
         }
-        return ones;
+
+        return sb.toString();
     }
 
+    /**
+     * Return the decode string for input
+     *
+     * @param input
+     * @return
+     */
+    static String Decode(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        int length = input.length();
+        for (int i = 0; i < length; i++) {
+            char current = input.charAt(i);
+            StringBuilder number_sb = new StringBuilder();
+
+            while (Character.isDigit(current)) {
+                number_sb.append(current);
+                i++;
+                current = input.charAt(i);
+            }
+
+            if (number_sb.toString().length() > 0 && current == '*') {
+                current = input.charAt(++i);
+                int number = Integer.parseInt(number_sb.toString());
+                for (int j = 0; j < number; j++) {
+                    sb.append(current);
+                }
+            } else {
+                sb.append(current);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * For test only
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        String[] array = new String[2];
-        List<String> list2 = Arrays.asList(array);
-        list2.add("test");
+        String input = "222222222222222222222222a**2222222222222dasda22222222222******!***!W!WQW!sadasdite7qw7egskjG";
+        String encode = Test.Encode(input);
+        String decode = Test.Decode(encode);
+        System.out.println(input + "->" + encode + "->" + decode);
+        System.out.println("success? " + input.equals(decode));
+        System.out.println("rate " + (double) encode.length() / decode.length());
     }
 }
--- src/onsite/Bianli.java
+++ src/onsite/Bianli.java
@@ -35,12 +35,10 @@ public class Bianli {
     public static void getCandidates() {
         Content c1 = new Content(1, 100);
         Content c2 = new Content(2, 80);
-        Content c3 = new Content(3, 75);
         area1.add(c1);
         area1.add(c2);
         area2.add(c1);
-        area3.add(c1);
-        area3.add(c3);
+        area2.add(new Content(3, 40));
         area1.add(null);
         area2.add(null);
         area3.add(null);
@@ -53,41 +51,41 @@ public class Bianli {
         getCandidates();
         String result = "";
         double max = 0;
-        for (int i = 0; i < area1.size(); i++) {
+        for (Content i : area1) {
             HashSet<Content> hash = new HashSet<>();
-            if (area1.get(i) != null) {
-                hash.add(area1.get(i));
+            if (i != null) {
+                hash.add(i);
             }
-            for (int j = 0; j < area2.size(); j++) {
-                if (hash.contains(area2.get(j))) {
+            for (Content j : area2) {
+                if (hash.contains(j)) {
                     continue;
                 }
-                if (area2.get(j) != null) {
-                    hash.add(area2.get(j));
+                if (j != null) {
+                    hash.add(j);
                 }
-                for (int k = 0; k < area3.size(); k++) {
-                    if (hash.contains(area3.get(k))) {
+                for (Content k : area3) {
+                    if (hash.contains(k)) {
                         continue;
                     }
-                    if (area3.get(k) != null) {
-                        hash.add(area3.get(k));
+                    if (k != null) {
+                        hash.add(k);
                     }
-                    for (int l = 0; l < area4.size(); l++) {
-                        if (hash.contains(area4.get(l))) {
+                    for (Content l : area4) {
+                        if (hash.contains(l)) {
                             continue;
                         }
-                        if (area4.get(l) != null) {
-                            hash.add(area4.get(l));
+                        if (l != null) {
+                            hash.add(l);
                         }
-                        for (int m = 0; m < area5.size(); m++) {
-                            if (hash.contains(area5.get(m))) {
+                        for (Content m : area5) {
+                            if (hash.contains(m)) {
                                 continue;
                             }
-                            if (area5.get(m) != null) {
-                                hash.add(area5.get(m));
+                            if (m != null) {
+                                hash.add(m);
                             }
-                            for (int n = 0; n < area6.size(); n++) {
-                                if (hash.contains(area6.get(n))) {
+                            for (Content n : area6) {
+                                if (hash.contains(n)) {
                                     continue;
                                 }
                                 double tmp = sumSixArea(i, j, k, l, m, n);
@@ -105,25 +103,25 @@ public class Bianli {
         System.out.println(max);
     }
 
-    public static double sumSixArea(int i, int j, int k, int l, int m, int n) {
+    public static double sumSixArea(Content i, Content j, Content k, Content l, Content m, Content n) {
         double result = 0;
-        if (area1.get(i) != null) {
-            result += area1.get(i).value * possibility[0];
+        if (i != null) {
+            result += i.value * possibility[0];
         }
-        if (area2.get(j) != null) {
-            result += area2.get(j).value * possibility[1];
+        if (j != null) {
+            result += j.value * possibility[1];
         }
-        if (area3.get(k) != null) {
-            result += area3.get(k).value * possibility[2];
+        if (k != null) {
+            result += k.value * possibility[2];
         }
-        if (area4.get(l) != null) {
-            result += area4.get(l).value * possibility[3];
+        if (l != null) {
+            result += l.value * possibility[3];
         }
-        if (area5.get(m) != null) {
-            result += area5.get(m).value * possibility[4];
+        if (m != null) {
+            result += m.value * possibility[4];
         }
-        if (area6.get(n) != null) {
-            result += area6.get(n).value * possibility[5];
+        if (n != null) {
+            result += n.value * possibility[5];
         }
         return result;
     }
