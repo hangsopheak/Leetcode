8bd8e1afd2eddc52864edb6fe7e5a64046851dc5
sow7 <wansongsong.jack@gmail.com>
2015-11-06 00:27:48 -0500

    11/6

---
+++ src/pock/countdouble.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pock;
+
+/**
+ *
+ * @author SONGSONG
+ */
+public class countdouble {
+
+    static int countDuplicates(int[] input) {
+        int result = 0;
+        for (int i : input) {
+            int j = Math.abs(i);
+            if (input[j - 1] < 0) {
+                result++;
+            } else {
+                input[j - 1] *= -1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] inputs = {1, 2, 2, 3, 4, 5, 5, 3, 1, 4};
+        int r = countDuplicates(inputs);
+        System.out.println(r);
+    }
+}
