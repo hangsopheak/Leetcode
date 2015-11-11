9ab2eaa1f857d9e551fadb85880eca61b43f9a74
sow7 <wansongsong.jack@gmail.com>
2015-11-11 12:48:27 -0500

    11/11

---
+++ src/BB/ValidateBSTree.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BB;
+
+import leetcode.TreeNode;
+
+/**
+ *
+ * @author SONGSONG
+ */
+public class ValidateBSTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
