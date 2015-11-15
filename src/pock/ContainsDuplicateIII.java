a96f4852e3cc1967cfe519467cd6ed20c48d7d9a
sow7 <wansongsong.jack@gmail.com>
2015-11-14 20:35:25 -0500

    11/14

--- src/pock/ContainsDuplicateIII.java
+++ src/pock/ContainsDuplicateIII.java
@@ -5,10 +5,31 @@
  */
 package pock;
 
+import java.util.HashMap;
+
 /**
  *
  * @author SONGSONG
  */
 public class ContainsDuplicateIII {
-    
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) {
+            return false;
+        }
+        int length = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            Integer tail = nums[i] / (t + 1);
+            if ((map.containsKey(tail) && Math.abs((long) map.get(tail) - nums[i]) <= t) || (map.containsKey(tail + 1) && map.get(tail + 1) - nums[i] <= t) || (map.containsKey(tail - 1) && nums[i] - map.get(tail - 1) <= t)) {
+                return true;
+            }
+            if (map.size() >= k) {
+                int remove = nums[i - k] / (t + 1);
+                map.remove(remove);
+            }
+            map.put(tail, nums[i]);
+        }
+        return false;
+    }
 }
