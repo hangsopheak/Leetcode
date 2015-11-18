0a2b8e451e7c98cd1c39d6b9a80a2c78c3c634cd
sow7 <wansongsong.jack@gmail.com>
2015-11-18 13:14:23 -0500

    11/18

--- src/avna/BFS.java
+++ src/avna/BFS.java
@@ -5,10 +5,36 @@
  */
 package avna;
 
+import java.util.*;
+import leetcode.TreeNode;
+
 /**
  *
  * @author SONGSONG
  */
 public class BFS {
-    
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> sub = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                if (queue.peek().left != null) {
+                    queue.add(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.add(queue.peek().right);
+                }
+                sub.add(queue.poll().val);
+            }
+            result.add(sub);
+        }
+        return result;
+    }
 }
